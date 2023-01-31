6aaf93d2c81b4098dbca2dc7c2d87efa9d2eec36
AT <101066547+UmbratheUmbreon@users.noreply.github.com>
2023-01-31 13:37:00 -0800

    init
    uh yeah yeah

---
+++ source/Main.hx
@@ -0,0 +1,35 @@
+package;
+
+import flixel.FlxGame;
+import openfl.display.FPS;
+import openfl.display.Sprite;
+import flixel.FlxG;
+
+class Main extends Sprite
+{
+	public function new()
+	{
+		super();
+		addChild(new FlxGame(0, 0, TitleState));
+
+		#if !mobile
+		addChild(new FPS(10, 3, 0xFFFFFF));
+		#end
+		#if cpp
+		cpp.vm.Gc.enable(true);
+		#end
+
+		FlxG.signals.preStateSwitch.add(() -> {
+			FlxG.bitmap.dumpCache();
+			FlxG.sound.destroy(false);
+			#if cpp
+			cpp.vm.Gc.run(true);
+			#end
+		});
+		FlxG.signals.postStateSwitch.add(() -> {
+			#if cpp
+			cpp.vm.Gc.run(false);
+			#end
+		});
+	}
+}
---
+++ source/PauseSubState.hx
@@ -0,0 +1,93 @@
+package;
+
+import flixel.group.FlxGroup.FlxTypedGroup;
+import Controls.Control;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxSubState;
+import flixel.input.keyboard.FlxKey;
+import flixel.util.FlxColor;
+
+class PauseSubState extends MusicBeatSubstate
+{
+	var daOptions:Array<String> = ['Resume Song', 'Restart Song', 'Exit'];
+	var grpOptions:FlxTypedGroup<Alphabet>;
+	var curSelected:Int = 0;
+	public function new(x:Float, y:Float)
+	{
+		super();
+		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
+		bg.alpha = 0.6;
+		bg.scrollFactor.set();
+		add(bg);
+
+		grpOptions = new FlxTypedGroup<Alphabet>();
+		add(grpOptions);
+
+		for (i in 0...daOptions.length)
+		{
+			var optionTxt:Alphabet = new Alphabet(0, (70 * i) + 30, daOptions[i], true, false);
+			optionTxt.isMenuItem = true;
+			optionTxt.targetY = i;
+			grpOptions.add(optionTxt);
+		}
+
+		changeSelection();
+
+		cameras = [FlxG.cameras.list[FlxG.cameras.list.length-1]];
+	}
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+
+		var upP = controls.UP_P;
+		var downP = controls.DOWN_P;
+		var accepted = controls.ACCEPT;
+
+		if (upP)
+		{
+			changeSelection(-1);
+		}
+		if (downP)
+		{
+			changeSelection(1);
+		}
+
+		/*if (FlxG.keys.justPressed.J)
+		{
+			PlayerSettings.player1.controls.replaceBinding(Control.LEFT, Keys, FlxKey.J, null);
+		}*/
+
+		if (accepted) {
+			switch (daOptions[curSelected]) {
+				case 'Resume Song':
+					close();
+				case 'Restart Song':
+					FlxG.resetState();
+				case 'Exit':
+					FlxG.switchState((PlayState.isStoryMode ? new StoryMenuState() : new FreeplayState()));
+			}
+		}
+	}
+
+	function changeSelection(change:Int = 0)
+	{
+		curSelected += change;
+
+		if (curSelected < 0)
+			curSelected = daOptions.length - 1;
+		if (curSelected >= daOptions.length)
+			curSelected = 0;
+
+		var bullShit:Int = 0;
+
+		for (item in grpOptions.members)
+		{
+			item.targetY = bullShit - curSelected;
+			bullShit++;
+			item.alpha = 0.6;
+			if (item.targetY == 0) item.alpha = 1;
+		}
+	}
+}
